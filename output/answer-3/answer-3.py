00af1cd788598f126a397c8aab7fc9c6f7cbe787
Charles Samuel R <rcharles.samuel99@gmail.com>
2022-04-21 20:19:56 +0530

    Moved to Python script

---
+++ output/answer-3/answer-3.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('/home/csr/Documents/internship-test2/input/question-3/main.csv')
+df.head()
+
+df.sort_values(by=['Red Cards', 'Yellow Cards'], ascending=False, inplace=True)
+
+cols = ['Team', 'Red Cards', 'Yellow Cards']
+df = df[cols]
+
+df.to_csv('main.csv')
